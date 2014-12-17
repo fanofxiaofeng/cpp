cfbf920e62ea6aa5e454785d27c652e20fb5649a
jinyang <jyjz2008@sjtu.edu.cn>
2014-12-17 18:53:07 +0800

    add 1931

---
+++ timus/1931/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	scanf("%d", &n);
+
+	int opt_cnt = 0;
+	int opt_pos = 1;
+	
+	int pos = 1;
+	int cnt = 0;
+
+	int current;
+	scanf("%d", &current);
+
+	for(int i = 2;i <= n;i++){
+		int temp;
+		scanf("%d", &temp);
+		if(temp < current){
+			current = temp;
+			cnt = 1;
+			pos = i;
+			if(cnt > opt_cnt){
+				opt_cnt = cnt;
+				opt_pos = pos;
+				// printf("already changed\n");
+			}
+		} else {
+			cnt++;
+			if(cnt > opt_cnt){
+				opt_cnt = cnt;
+				opt_pos = pos;
+				// printf("add\n");
+			}
+		}
+	}
+	printf("%d\n", opt_pos);
+	return 0;
+}
+
